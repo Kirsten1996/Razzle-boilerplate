7b6cf1c1643fb033aaf17d704ae1ba9ebd575cc2
Kirsten Stemmet <kirsten@bnry.local>
2019-04-30 12:07:01 +0200

    change

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 class Home extends Component {
   render() {
-    return <div>Hi Razzle</div>;
+    return (
+      <div>
+        <p>Hey</p>
+      </div>
+    );
   }
 }
 
